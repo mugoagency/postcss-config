cfa44e5eea3d181a4670d168f47a0632e7fe454e
ꜱ ʜ ᴀ ᴩ ᴇ <40563980+shapedigital@users.noreply.github.com>
2022-05-25 14:17:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = {
+const banner = '! Do not modify this file. Make changes via the "src" directory and recompile.';
+
+const config = {
   plugins: [
     "postcss-simple-vars",
     {
@@ -18,10 +20,16 @@ module.exports = {
     "postcss-round-subpixels",
     "postcss-easings",
     {
+      "postcss-banner": {
+        banner: banner,
+        inline: true
+      }
+    },
+    {
       "cssnano": {
         preset: ['lite', {
           discardComments: {
-            removeAll: true,
+            removeAll: false,
           },
         }]
       },
