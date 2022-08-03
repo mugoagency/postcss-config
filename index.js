bc0cc3d101505dec0d454701d7757fe9573c6aa6
ꜱ ʜ ᴀ ᴩ ᴇ <40563980+shapedigital@users.noreply.github.com>
2022-08-03 11:28:19 +0100

    Add PX to Rem conversion on select properties

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const banner = '! Do not modify this file. Make changes via the "src" directory, then recompile.';
+const banner = '! Do not modify this file. Make changes via the "_src" directory, then recompile.';
 
 module.exports = {
   plugins: [
     "postcss-simple-vars",
+    {
+      "postcss-pxtorem": {
+        replace: true,
+        propList: [
+          'font', 
+          'font-size', 
+          'line-height', 
+          'padding', 
+          'padding-top',
+          'padding-right',
+          'padding-bottom',
+          'padding-left',
+          'margin-top',
+          'margin-right',
+          'margin-bottom',
+          'margin-left',
+          'padding-inline-start',
+          'padding-inline-end',
+          'padding-block-start',
+          'padding-block-end',
+          'margin-inline-start',
+          'margin-inline-end',
+          'margin-block-start',
+          'margin-block-end',
+          'margin', 
+          'letter-spacing' , 
+          '--*'],
+      },
+    },
     {
       "postcss-easy-import": {
         extensions: ".pcss",
