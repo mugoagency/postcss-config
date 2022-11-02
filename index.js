f89ff03873f13f449ecb46940736595be4801704
ꜱ ʜ ᴀ ᴩ ᴇ <40563980+shapedigital@users.noreply.github.com>
2022-11-02 12:10:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ module.exports = {
     {
       "postcss-inline-svg": {
         paths: ["_src/svgs"],
+        removeFill: true, // Provide these on case-by-case basis.
+        removeStroke: true,
       },
     },
     {
